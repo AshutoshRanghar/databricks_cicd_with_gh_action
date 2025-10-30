656fb615a19e3ba1b72e218288674dc88d786101
Ashutosh <ashutoshranghar1@gmail.com>
2025-10-30 17:15:35 +0530

    New changes to setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,15 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='pyspark_project',
-    version='1.0.0',
+    name="pyspark_project",
+    version="1.0.0",
     packages=find_packages(),
-    install_requires=[
-        'pyspark>=3.3.0',
-        'delta-spark>=2.4.0'
-    ],
     entry_points={
         'console_scripts': [
-            'run-etl=pyspark_project.etl_job:main'
-        ]
-    }
+            'etl_job = etl_job:main',
+        ],
+    },
+    install_requires=[
+        "pyspark>=3.0.0",
+    ],
 )
